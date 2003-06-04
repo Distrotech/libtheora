1ee5f2f46f6cc4f5887bf2075cb1944d520b4f7e
mauricio <mauricio@xiph.org>
2003-06-04 01:31:46 +0000

    oops, forgot to add the header file. part of previous commit for packed huffman header, submitted by danxor
    git-svn-id: http://svn.xiph.org/trunk/theora@4871 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ lib/toplevel.h
@@ -0,0 +1,24 @@
+/********************************************************************
+ *                                                                  *
+ * THIS FILE IS PART OF THE OggTheora SOFTWARE CODEC SOURCE CODE.   *
+ * USE, DISTRIBUTION AND REPRODUCTION OF THIS LIBRARY SOURCE IS     *
+ * GOVERNED BY A BSD-STYLE SOURCE LICENSE INCLUDED WITH THIS SOURCE *
+ * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
+ *                                                                  *
+ * THE OggVorbis SOURCE CODE IS (C) COPYRIGHT 1994-2002             *
+ * by the Xiph.Org Foundation http://www.xiph.org/                  *
+ *                                                                  *
+ ********************************************************************
+
+  function: 
+  last mod: $Id: toplevel.h,v 1.1 2003/06/04 01:31:46 mauricio Exp $
+
+ ********************************************************************/
+
+/*from dbm huffman pack patch*/
+
+extern void write_Qtables(oggpack_buffer* opb);
+extern void write_HuffmanSet(oggpack_buffer* opb, HUFF_ENTRY **hroot);
+extern void read_Qtables(oggpack_buffer* opb);
+extern void read_HuffmanSet(oggpack_buffer* opb);
+extern void InitHuffmanSetPlay( PB_INSTANCE *pbi );
