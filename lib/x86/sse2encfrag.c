Distrotech/libtheora

commit 0ef516099d1eee3e05376add15f8239dc494fc83

Remove some useless accounting from the SSE2 SATD that was left over from the  MMX version it was derived from.
git-svn-id: http://svn.xiph.org/experimental/derf/theora-ptalarbvorm@16967 0101bb08-14d6-0310-b084-bc0e0c8e3800

lib/x86/sse2encfrag.c
@@ -72,13 +72,9 @@
  "neg %[ref_ystride]\n\t" \
  "psubw %%xmm0,%%xmm6\n\t" \
  "movq (%[ref],%[ref_ystride]),%%xmm0\n\t" \
- "lea (%[src],%[src_ystride],8),%[src]\n\t" \
  "punpcklbw %%xmm0,%%xmm7\n\t" \
- "neg %[src_ystride]\n\t" \
  "punpcklbw %%xmm0,%%xmm0\n\t" \
- "lea (%[ref],%[ref_ystride],8),%[ref]\n\t" \
  "psubw %%xmm0,%%xmm7\n\t" \
- "neg %[ref_ystride]\n\t" \
  "movdqa (%[buf]),%%xmm0\n\t" \
 
 /*Load an 8x8 array of pixel values from %[src] into %%xmm0...%%xmm7.*/
@@ -198,20 +194,20 @@
  "pmaxsw %%xmm3,%%xmm2\n\t" \
  "pmaxsw %%xmm1,%%xmm0\n\t" \
  "paddw %%xmm3,%%xmm6\n\t" \
- "paddw %%xmm5,%%xmm1\n\t" \
  "movdqa 0x10(%[buf]),%%xmm3\n\t" \
+ "paddw %%xmm5,%%xmm1\n\t" \
+ "movdqa (%[buf]),%%xmm5\n\t" \
 
 /*Performs the second part of the final stage of the Hadamard transform and
    summing of absolute values.*/
 #define OC_HADAMARD_C_ABS_ACCUM_B_8x8 \
  "#OC_HADAMARD_C_ABS_ACCUM_B_8x8\n\t" \
  "paddsw %%xmm7,%%xmm6\n\t" \
- "movdqa (%[buf]),%%xmm5\n\t" \
  "paddsw %%xmm7,%%xmm1\n\t" \
  "psubw %%xmm6,%%xmm2\n\t" \
  "psubw %%xmm1,%%xmm0\n\t" \
  /*xmm7={1}x4 (needed for the horizontal add that follows) \
-   xmm0+=xmm2+xmm4+max(abs(xmm1),abs(xmm3))-0x7FFF*/ \
+   xmm0+=xmm2+xmm4+max(abs(xmm3),abs(xmm5))-0x7FFF*/ \
  "movdqa %%xmm3,%%xmm6\n\t" \
  "pmaxsw %%xmm5,%%xmm3\n\t" \
  "paddw %%xmm2,%%xmm0\n\t" \

commit 09c2270f1407b4bec1d40f99697120e8f6fabd3b

Be more explicit about register classes in oc_enc_frag_satd2_sse2() to prevent  gcc from doing silly things like using %rsp for an input operand that we  clobber. We can't mark them all as output operands, because then they couldn't be  aliased with actual outputs, and there wouldn't be enough registers on x86-32. Fixes a crash introduced in r16967 for some compilers/options.
git-svn-id: http://svn.xiph.org/experimental/derf/theora-ptalarbvorm@16970 0101bb08-14d6-0310-b084-bc0e0c8e3800

lib/x86/sse2encfrag.c
@@ -278,10 +278,12 @@ static unsigned oc_int_frag_satd_sse2(unsigned *_dc,
     /*Note that _src_ystride and _ref_ystride must be given non-overlapping
        constraints, otherewise if gcc can prove they're equal it will allocate
        them to the same register (which is bad); _src and _ref face a similar
-       problem, though those are never actually the same.*/
+       problem.
+      All four are destructively modified, but if we list them as output
+       constraints, gcc can't alias them with other outputs.*/
     :[ret]"=a"(ret),[dc]"=d"(dc),[buf]"+r"(bufp)
-    :[src]"r"(_src),[src_ystride]"c"((ptrdiff_t)_src_ystride),
-     [ref]"r"(_ref),[ref_ystride]"d"((ptrdiff_t)_ref_ystride)
+    :[src]"S"(_src),[src_ystride]"c"((ptrdiff_t)_src_ystride),
+     [ref]"a"(_ref),[ref_ystride]"d"((ptrdiff_t)_ref_ystride)
     /*We have to use neg, so we actually clobber the condition codes for once
        (not to mention sub, and add).*/
     :"cc"