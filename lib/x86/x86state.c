d060d3339ebec4161339fa1a2918c18338cecccf
Ralph Giles <giles@xiph.org>
2010-09-22 16:46:18 +0000

    Correct a typo in the name of mmx loop filter initialization functions.
    git-svn-id: http://svn.xiph.org/experimental/derf/theora-ptalarbvorm@17421 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- lib/x86/x86state.c
+++ lib/x86/x86state.c
@@ -73,14 +73,14 @@ void oc_state_accel_init_x86(oc_theora_state *_state){
     _state->opt_vtable.frag_recon_inter2=oc_frag_recon_inter2_mmx;
     _state->opt_vtable.idct8x8=oc_idct8x8_mmx;
     _state->opt_vtable.state_frag_recon=oc_state_frag_recon_mmx;
-    _state->opt_vtable.loop_filter_init=oc_state_loop_filter_init_mmx;
+    _state->opt_vtable.loop_filter_init=oc_loop_filter_init_mmx;
     _state->opt_vtable.state_loop_filter_frag_rows=
      oc_state_loop_filter_frag_rows_mmx;
     _state->opt_vtable.restore_fpu=oc_restore_fpu_mmx;
     _state->opt_data.dct_fzig_zag=OC_FZIG_ZAG_MMX;
   }
   if(_state->cpu_flags&OC_CPU_X86_MMXEXT){
-    _state->opt_vtable.loop_filter_init=oc_state_loop_filter_init_mmxext;
+    _state->opt_vtable.loop_filter_init=oc_loop_filter_init_mmxext;
     _state->opt_vtable.state_loop_filter_frag_rows=
      oc_state_loop_filter_frag_rows_mmxext;
   }
